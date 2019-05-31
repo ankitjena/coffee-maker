81d39ee8cf9db9a6f542b356aed09a6a639d1049
ankitjena <ankitjena13@gmail.com>
2019-05-31 10:27:27 +0530

    feature: cli to add recipe and brew coffee

--- index.ts
+++ index.ts
@@ -1,17 +1,77 @@
-import { CoffeeController } from './src/CoffeeController'
-import { UserInput } from './src/UserInput'
-import { CoffeRecipe } from './src/Recipe'
+#! /usr/bin/env node
+import { promisify } from 'util'
+import { writeFile } from 'fs'
+import chalk from 'chalk'
+import { CoffeeController } from './src/CoffeeController';
+
+const inquirer = require('inquirer')
 
 /**
- * Main function which starts up the coffee machine, provides inputs and brews coffee
+ * cli method supports async/await
  */
-const main = async() => {
-  const coffeeInstance = new CoffeeController()
-  const recipe = new CoffeRecipe()
-  coffeeInstance.init({milk: 200, water: 300, coffeePowder: 100})
-  await coffeeInstance.brew(recipe.Black)
-  await coffeeInstance.brew(recipe.Capucchino)
-  await coffeeInstance.brew(recipe.Espresso)
-}
+const cli = async(): Promise<void> => {
+  /**
+   * Async writefile and file path
+   */
+  const writeFileAsync = promisify(writeFile)
+  const recipesPath = `${process.cwd()}/src/Recipes.json`
 
-main()
+  const recipes: object = require('./src/Recipes.json')
+  /**
+   * Inquirer question to be asked
+   */
+  const question: object[] = [
+    {
+      type: "list",
+      name: "recipe",
+      message: "Choose a available recipe",
+      choices: Object.keys(recipes),
+    }
+  ]
+
+  const argv = require('yargs')
+    .usage('Usage: $0 <command> [options')
+    /**
+     * Command to add recipe.
+     * Takes 4 arguments, name: string
+     * milk: number, water: number, coffee: number
+     */
+    .command('add-recipe', 'Add a recipe to the list of recipes', (yargs) => {
+      return yargs.option('name')
+                  .option('milk')
+                  .option('water')
+                  .option('coffee')
+    },
+    async ({name, milk, water, coffee}) => {
+      const newRecipeName: string = name
+      const newRecipeDetails: object = {"Water": water, "Milk": milk, "Coffee": coffee}
+      recipes[newRecipeName] = newRecipeDetails
+      await writeFileAsync(recipesPath, JSON.stringify(recipes, undefined, 2))
+      console.log(chalk.green("Recipe added"))
+    })
+    /**
+     * Brews coffee based on the recipe provided. If no recipe provided then
+     * asks the user to select one from the existing recipes.
+     */
+    .command('brew', 'Brew coffee', (yargs) => {
+      return yargs
+    },async (yargs) => {
+      let name = yargs._[1]
+      if(name && !recipes.hasOwnProperty(name)) {
+        //throw error
+        throw new Error(chalk.red("No such recipe"))
+      }
+      if(!name) {
+        let answer = await inquirer.prompt(question)
+        name = answer.recipe
+      }
+
+      const coffeeInstance = new CoffeeController()
+      coffeeInstance.init({milk: 200, water: 300, coffee: 100})
+      await coffeeInstance.brew(recipes[name])
+      
+    })
+    .help('h')
+    .alias('h', 'help').argv
+}
+cli()
