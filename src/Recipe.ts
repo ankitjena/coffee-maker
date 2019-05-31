e795cd8914d1b29462d1775c00d0f95b9d0c05af
ankitjena <ankitjena13@gmail.com>
2019-05-31 10:27:27 +0530

    feature: cli to add recipe and brew coffee

--- src/Recipe.ts
+++ src/Recipe.ts
@@ -12,18 +12,18 @@ export interface Recipe {
 /**
  * Class with various instances of coffee recipes
  */
-export class CoffeRecipe {
-  Latte: Recipe
-  Black: Recipe
-  Capucchino: Recipe
-  Espresso: Recipe
-  /**
-   * instantiate different coffee recipes
-   */
-  constructor() {
-    this.Latte = {Water: 40, Milk: 20, Coffee: 20}
-    this.Black = {Water: 60, Milk: 0, Coffee: 20}
-    this.Capucchino = {Water: 30, Milk: 40, Coffee: 20}
-    this.Espresso = {Water: 30, Milk: 30, Coffee: 20}
-  }
-}
+// export class CoffeRecipe {
+//   Latte: Recipe
+//   Black: Recipe
+//   Capucchino: Recipe
+//   Espresso: Recipe
+//   /**
+//    * instantiate different coffee recipes
+//    */
+//   constructor() {
+//     this.Latte = {Water: 40, Milk: 20, Coffee: 20}
+//     this.Black = {Water: 60, Milk: 0, Coffee: 20}
+//     this.Capucchino = {Water: 30, Milk: 40, Coffee: 20}
+//     this.Espresso = {Water: 30, Milk: 30, Coffee: 20}
+//   }
+// }
